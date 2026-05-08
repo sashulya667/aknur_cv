995a7250efbb2142b0243e525bba47b432b2aa5b
Sanzhar Umbet <sanzhar.umbet@nu.edu.kz>
2026-05-08 09:43:02 +0200

    Add meta tags and headers to prevent indexing

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex, nofollow, noarchive, nosnippet',
+          },
+        ],
+      },
+    ]
+  },
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="robots" content="noindex, nofollow, noarchive, nosnippet" />
+        <meta
+          name="googlebot"
+          content="noindex, nofollow, noarchive, nosnippet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
